e9ec701d12ee8f40970770ecd46f6827479539fd
iago oliveira nascimento <iago.onascimento@hotmail.com>
2023-07-06 22:31:17 -0300

    docker funfando

--- src/config/mysql.config.js
+++ src/config/mysql.config.js
@@ -4,11 +4,11 @@ import dotenv from 'dotenv'
 dotenv.config();
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database : process.env.DB_NAME,
+  host: 'mysqldb',
+  port: 3306,
+  user: 'root',
+  password: 'root',
+  database : 'filaru',
   connectionLimit: 20
 });
 

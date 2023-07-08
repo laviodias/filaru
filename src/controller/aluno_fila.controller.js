ef30f3682c03c281e729855403786892e607b550
Lávio <laviovale@gmail.com>
2023-07-08 18:51:55 -0300

    added automatic position

--- src/controller/aluno_fila.controller.js
+++ src/controller/aluno_fila.controller.js
@@ -5,7 +5,7 @@ import QUERY from "../query/aluno_fila.query.js";
 import HttpStatus from "../util/httpStatus.js";
 
 export const findAll = (req, res) => {
-  logger.info("GET /aluno-fila");
+  logger.info("GET /aluno_fila");
   database.query(QUERY.GET_ALL, (error, results) => {
     if (!results) {
       res
@@ -34,42 +34,62 @@ export const findAll = (req, res) => {
 };
 
 export const create = (req, res) => {
-  logger.info("CREATE /aluno-fila/" + Object.values(req.body));
-  database.query(QUERY.CREATE, Object.values(req.body), (error, results) => {
-    if (error) {
-      logger.error(error.message);
+  logger.info("CREATE /aluno_fila/" + Object.values(req.body));
+
+  database.query(QUERY.GET_LAST_POSITION, req.body.codigo_fila, (error, results) => {
+    if (!results) {
       res
-        .status(HttpStatus.BAD_REQUEST.code)
+        .status(HttpStatus.OK.code)
         .send(
           new Response(
-            HttpStatus.BAD_REQUEST.code,
-            HttpStatus.BAD_REQUEST.status,
-            "Error creating student_queue",
+            HttpStatus.OK.code,
+            HttpStatus.OK.status,
+            "No student_queues found",
             null
           )
         );
     } else {
-      const student_queue = {
-        aluno_matricula: req.body.alunoMatricula,
-        codigo_fila: req.body.codigoFila,
-        posicao: req.body.posicao,
-      };
-      res
-        .status(HttpStatus.CREATED.code)
-        .send(
-          new Response(
-            HttpStatus.CREATED.code,
-            HttpStatus.CREATED.status,
-            "student_queue created",
-            student_queue
-          )
-        );
+      const posicao = (results[0].posicao || 0) + 1;
+      req.body.posicao = posicao;
+      database.query(QUERY.CREATE, Object.values(req.body), (error, results) => {
+        if (error) {
+          logger.error(error.message);
+          res
+            .status(HttpStatus.BAD_REQUEST.code)
+            .send(
+              new Response(
+                HttpStatus.BAD_REQUEST.code,
+                HttpStatus.BAD_REQUEST.status,
+                "Error creating student_queue",
+                null
+              )
+            );
+        } else {
+          const student_queue = {
+            aluno_matricula: req.body.alunoMatricula,
+            codigo_fila: req.body.codigoFila,
+            posicao: req.body.posicao,
+          };
+          res
+            .status(HttpStatus.CREATED.code)
+            .send(
+              new Response(
+                HttpStatus.CREATED.code,
+                HttpStatus.CREATED.status,
+                "student_queue created",
+                student_queue
+              )
+            );
+        }
+      });
     }
   });
+
+
 };
 
 export const findOne = (req, res) => {
-  logger.info("GET /aluno-fila/");
+  logger.info("GET /aluno_fila/");
   database.query(QUERY.GET_ONE, Object.values(req.params), (error, results) => {
     if (!results) {
       res
@@ -98,7 +118,7 @@ export const findOne = (req, res) => {
 };
 
 export const remove = (req, res) => {
-  logger.info("DELETE /aluno-fila/");
+  logger.info("DELETE /aluno_fila/");
   database.query(QUERY.DELETE, Object.values(req.params), (error, results) => {
     if (error) {
       logger.error(error.message);
--- src/query/aluno_fila.query.js
+++ src/query/aluno_fila.query.js
@@ -3,6 +3,7 @@ const QUERY = {
   GET_ONE: 'SELECT * FROM aluno_fila WHERE aluno_matricula = ? AND codigo_fila = ?',
   CREATE: 'INSERT INTO aluno_fila (aluno_matricula, codigo_fila, posicao) VALUES (?, ?, ?)',
   DELETE: 'DELETE FROM aluno_fila WHERE aluno_matricula = ? AND codigo_fila = ?',
+  GET_LAST_POSITION: 'SELECT posicao FROM aluno_fila WHERE codigo_fila = ? ORDER BY posicao DESC LIMIT 1',
 }
 
 export default QUERY;
